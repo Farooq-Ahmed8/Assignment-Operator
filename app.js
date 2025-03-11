13e2fbb31aa0bb36777bb1a67c49c71a5ca681a5
Farooq Ahmed <03174748082ali@gmail.com>
2025-03-11 15:29:35 +0500

    my second commit

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //Assignment Operator
-// =
+
+//=
+
 // +=
+var num = 50;
+document.write("50 += 30:  ", num += 30);
+
 // -=
+var num = 50;
+document.write("50 += 30:  ", num += 30);
+
 // /=
+var num = 50;
+document.write("50 += 30:  ", num += 30);
+
 // *=
 var num = 50;
 document.write("50 += 30:  ", num += 30);
